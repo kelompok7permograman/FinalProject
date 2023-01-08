7a2c3bafa068fcdecc36db25f66689d92fcc1145
samagomenne@gmail.com <samagomenne@gmail.com>
2023-01-08 22:15:14 +0700

    pagecontent

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace FinalProject.Controllers
             return View();
         }
 
+        public IActionResult PageContent()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
